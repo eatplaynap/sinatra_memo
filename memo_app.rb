2d0d03b731e9b91c326e3a3eb4f70d41119b88ec
eatplaynap <eatplaynap329@gmail.com>
2021-05-30 17:30:31 +0900

    自動変換されてしまったダブルクオートをシングルクオートに変換

--- memo_app.rb
+++ memo_app.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require "sinatra"
-require "sinatra/reloader"
-require "json"
-require "securerandom"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+require 'securerandom'
 
 use Rack::MethodOverride
 
@@ -15,53 +15,53 @@ helpers do
   end
 end
 
-get "/memos" do
-  @title = "一覧"
-  file_names = Dir.glob("*.json")
+get '/memos' do
+  @title = '一覧'
+  file_names = Dir.glob('*.json')
   @names = file_names.map do |file_name|
     convert_json_into_hash(file_name)
   end
   erb :index
 end
 
-get "/memos/new" do
-  @title = "新規作成"
+get '/memos/new' do
+  @title = '新規作成'
   erb :new
 end
 
-post "/memos/new" do
+post '/memos/new' do
   @memo_title = params[:memo_title]
   @article = params[:article]
-  hash = { "id" => SecureRandom.uuid, "memo_title" => @memo_title, "article" => @article }
-  File.open("memo.#{hash["id"]}.json", "w") do |file|
+  hash = { 'id' => SecureRandom.uuid, 'memo_title' => @memo_title, 'article' => @article }
+  File.open("memo.#{hash['id']}.json", 'w') do |file|
     file.puts JSON.pretty_generate(hash)
   end
-  redirect to("/memos")
+  redirect to('/memos')
 end
 
-get "/memos/:id" do
-  @title = "詳細"
+get '/memos/:id' do
+  @title = '詳細'
   convert_json_into_hash(params[:id].to_s)
   erb :detail
 end
 
-get "/memos/:id/edit" do
-  @title = "編集"
+get '/memos/:id/edit' do
+  @title = '編集'
   convert_json_into_hash(params[:id].to_s)
   erb :edit
 end
 
-patch "/memos/:id" do
+patch '/memos/:id' do
   hash = convert_json_into_hash(params[:id].to_s)
-  hash["memo_title"] = params[:memo_title]
-  hash["article"] = params[:article]
-  File.open(params[:id].to_s, "w") do |file|
+  hash['memo_title'] = params[:memo_title]
+  hash['article'] = params[:article]
+  File.open(params[:id].to_s, 'w') do |file|
     JSON.dump(hash, file)
   end
   redirect to("/memos/#{params[:id]}")
 end
 
-delete "/memos/:id" do
+delete '/memos/:id' do
   File.delete(params[:id].to_s)
-  redirect to("/memos")
+  redirect to('/memos')
 end
