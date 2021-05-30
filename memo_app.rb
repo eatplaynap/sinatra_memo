b49adf5b6567f43633babecd351af399815a0f85
eatplaynap <eatplaynap329@gmail.com>
2021-05-30 14:08:39 +0900

    rubocop 2回目

--- memo_app.rb
+++ memo_app.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
@@ -8,13 +10,13 @@ use Rack::MethodOverride
 helpers do
   def convert_JSON_into_hash(params)
     File.open(params) do |file|
-        JSON.parse(file.read)
-      end
+      JSON.parse(file.read)
     end
- end
+  end
+end
 
 get '/memos' do
-  file_names = Dir.glob("*.json")
+  file_names = Dir.glob('*.json')
   @names = file_names.map do |file_name|
     convert_JSON_into_hash(file_name)
   end
@@ -28,11 +30,11 @@ end
 post '/memos/new' do
   @memo_title = params[:memo_title]
   @article = params[:article]
-  hash = { "id" => SecureRandom.uuid, "memo_title" => @memo_title, "article" => @article }
-  File.open("memo.#{hash["id"]}.json","w") do |file|
+  hash = { 'id' => SecureRandom.uuid, 'memo_title' => @memo_title, 'article' => @article }
+  File.open("memo.#{hash['id']}.json", 'w') do |file|
     file.puts JSON.pretty_generate(hash)
   end
-  redirect to ('/memos')
+  redirect to('/memos')
 end
 
 get '/memos/:id' do
@@ -52,13 +54,13 @@ patch '/memos/:id' do
   hash = convert_JSON_into_hash(params[:id].to_s)
   hash['memo_title'] = params[:memo_title]
   hash['article'] = params[:article]
-  File.open("#{params[:id]}","w") do |file|
+  File.open(params[:id].to_s, 'w') do |file|
     JSON.dump(hash, file)
   end
-  redirect to ("/memos/#{params[:id]}")
+  redirect to("/memos/#{params[:id]}")
 end
 
 delete '/memos/:id' do
-  File.delete("#{params[:id]}")
-  redirect to ('/memos')
+  File.delete(params[:id].to_s)
+  redirect to('/memos')
 end
