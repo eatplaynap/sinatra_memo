1595251c32a6980f2baf0aed6e195e8780b7bc70
eatplaynap <eatplaynap329@gmail.com>
2021-06-08 15:11:05 +0900

    datastrageディレクトリを作成しディレクトリトラバーサル対策

--- memo_app.rb
+++ memo_app.rb
@@ -9,7 +9,7 @@ use Rack::MethodOverride
 
 helpers do
   def convert_json_into_hash(params)
-    @memo_info = File.open(params) do |file|
+    @memo_info = File.open("datastrage/"+File.basename(params)) do |file|
       JSON.parse(file.read)
     end
   end
@@ -21,7 +21,7 @@ end
 
 get '/memos' do
   @title = '一覧'
-  file_names = Dir.glob('*.json')
+  file_names = Dir.glob('datastrage/*.json')
   @memos = file_names.map do |file_name|
     convert_json_into_hash(file_name)
   end
@@ -37,7 +37,7 @@ post '/memos' do
   memo_title = params[:memo_title]
   article = params[:article]
   hash = { 'id' => SecureRandom.uuid, 'memo_title' => memo_title, 'article' => article }
-  File.open("memo.#{hash['id']}.json", 'w') do |file|
+  File.open("datastrage/memo.#{hash['id']}.json", 'w') do |file|
     file.puts JSON.pretty_generate(hash)
   end
   redirect to('/memos')
@@ -45,27 +45,27 @@ end
 
 get '/memos/:id' do
   @title = '詳細'
-  convert_json_into_hash(params[:id].to_s)
+  convert_json_into_hash(params[:id])
   erb :detail
 end
 
 get '/memos/:id/edit' do
   @title = '編集'
-  convert_json_into_hash(params[:id].to_s)
+  convert_json_into_hash(params[:id])
   erb :edit
 end
 
 patch '/memos/:id' do
-  hash = convert_json_into_hash(params[:id].to_s)
+  hash = convert_json_into_hash(params[:id])
   hash['memo_title'] = params[:memo_title]
   hash['article'] = params[:article]
-  File.open(params[:id].to_s, 'w') do |file|
+  File.open("datastrage/#{params[:id]}", 'w') do |file|
     JSON.dump(hash, file)
   end
   redirect to("/memos/#{params[:id]}")
 end
 
 delete '/memos/:id' do
-  File.delete(params[:id].to_s)
+  File.delete("datastrage/#{params[:id]}")
   redirect to('/memos')
 end
