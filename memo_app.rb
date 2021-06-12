7219489670817c768ac785a234c3ef8b8eeb1438
eatplaynap <eatplaynap329@gmail.com>
2021-06-12 21:14:54 +0900

    rubocop OK済み

--- memo_app.rb
+++ memo_app.rb
@@ -2,13 +2,11 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-require 'json'
-require 'securerandom'
 require 'pg'
 
 use Rack::MethodOverride
 
-conn = PG.connect( dbname: 'sinatra_memo_db' , user: "eatplaynap" )
+conn = PG.connect(dbname: 'sinatra_memo_db', user: 'eatplaynap')
 
 helpers do
   def h(text)
@@ -18,10 +16,9 @@ end
 
 get '/memos' do
   @title = '一覧'
-  conn.prepare("show","SELECT * FROM memo ORDER BY $1")
-  @memos = conn.exec_prepared("show", ['id'])
+  conn.prepare('show', 'SELECT * FROM memo ORDER BY $1')
+  @memos = conn.exec_prepared('show', ['id'])
   conn.exec('DEALLOCATE show')
-  @memo
   erb :index
 end
 
@@ -33,49 +30,41 @@ end
 post '/memos' do
   memo_title = params[:memo_title]
   article = params[:article]
-  conn.prepare("post","INSERT INTO memo(memo_title, article)
-  VALUES($1, $2)" )
-  posting = conn.exec_prepared("post",["#{memo_title}", "#{article}"])
+  conn.prepare('post', "INSERT INTO memo(memo_title, article)
+  VALUES($1, $2)")
+  conn.exec_prepared('post', [memo_title.to_s, article.to_s])
   conn.exec('DEALLOCATE post')
-  posting
   redirect to('/memos')
 end
 
 get '/memos/:id' do
   @title = '詳細'
-  conn.prepare("find","SELECT * FROM memo WHERE id = $1")
-  @memo_info = conn.exec_prepared("find", ["#{params[:id]}"])
+  conn.prepare('find', 'SELECT * FROM memo WHERE id = $1')
+  @memo_info = conn.exec_prepared('find', [params[:id].to_s])
   conn.exec('DEALLOCATE find')
-  @memo_info
   erb :detail
 end
 
 get '/memos/:id/edit' do
   @title = '編集'
-  conn.prepare("edit", "SELECT * FROM memo WHERE id = $1")
-  @memo_info = conn.exec_prepared("edit",["#{params[:id]}"] )
+  conn.prepare('edit', 'SELECT * FROM memo WHERE id = $1')
+  @memo_info = conn.exec_prepared('edit', [params[:id].to_s])
   conn.exec('DEALLOCATE edit')
-  @memo_info
   erb :edit
 end
 
 patch '/memos/:id' do
   memo_title = params[:memo_title]
   article = params[:article]
-  conn.prepare("patch", "UPDATE memo
-    SET memo_title = $1,
-        article = $2
-      WHERE id =  $3 ")
-  patching = conn.exec_prepared("patch",["#{memo_title}", "#{article}","#{params[:id]}"])
+  conn.prepare('patch', 'UPDATE memo SET memo_title = $1, article = $2 WHERE id = $3')
+  conn.exec_prepared('patch', [memo_title.to_s, article.to_s, params[:id].to_s])
   conn.exec('DEALLOCATE patch')
-  patching
   redirect to("/memos/#{params[:id]}")
 end
 
 delete '/memos/:id' do
-  conn.prepare("delete", "DELETE FROM memo WHERE id = $1")
-  deleting = conn.exec_prepared("delete",["#{params[:id]}"])
+  conn.prepare('delete', 'DELETE FROM memo WHERE id = $1')
+  conn.exec_prepared('delete', [params[:id].to_s])
   conn.exec('DEALLOCATE delete')
-  deleting
   redirect to('/memos')
 end
