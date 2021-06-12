d19a7e0fd03557f8d60a5da5618eb74eab324f27
eatplaynap <eatplaynap329@gmail.com>
2021-06-12 20:57:56 +0900

    SQLインジェクション対策

--- memo_app.rb
+++ memo_app.rb
@@ -18,7 +18,10 @@ end
 
 get '/memos' do
   @title = '一覧'
-  @memos = conn.exec( "SELECT * FROM memo ORDER BY id" )
+  conn.prepare("show","SELECT * FROM memo ORDER BY $1")
+  @memos = conn.exec_prepared("show", ['id'])
+  conn.exec('DEALLOCATE show')
+  @memo
   erb :index
 end
 
@@ -30,35 +33,49 @@ end
 post '/memos' do
   memo_title = params[:memo_title]
   article = params[:article]
-  conn.exec("INSERT INTO memo(memo_title, article)
-    VALUES('#{memo_title}', '#{article}')")
+  conn.prepare("post","INSERT INTO memo(memo_title, article)
+  VALUES($1, $2)" )
+  posting = conn.exec_prepared("post",["#{memo_title}", "#{article}"])
+  conn.exec('DEALLOCATE post')
+  posting
   redirect to('/memos')
 end
 
 get '/memos/:id' do
   @title = '詳細'
-  @memo_info = conn.exec( "SELECT * FROM memo WHERE id = #{params[:id]}" )
+  conn.prepare("find","SELECT * FROM memo WHERE id = $1")
+  @memo_info = conn.exec_prepared("find", ["#{params[:id]}"])
+  conn.exec('DEALLOCATE find')
+  @memo_info
   erb :detail
 end
 
 get '/memos/:id/edit' do
   @title = '編集'
-  @memo_info = conn.exec( "SELECT * FROM memo WHERE id = #{params[:id]}" )
+  conn.prepare("edit", "SELECT * FROM memo WHERE id = $1")
+  @memo_info = conn.exec_prepared("edit",["#{params[:id]}"] )
+  conn.exec('DEALLOCATE edit')
+  @memo_info
   erb :edit
 end
 
 patch '/memos/:id' do
   memo_title = params[:memo_title]
   article = params[:article]
-  conn.exec( "UPDATE memo
-  SET memo_title = '#{memo_title}',
-      article = '#{article}'
-    WHERE id =  #{params[:id]} " )
+  conn.prepare("patch", "UPDATE memo
+    SET memo_title = $1,
+        article = $2
+      WHERE id =  $3 ")
+  patching = conn.exec_prepared("patch",["#{memo_title}", "#{article}","#{params[:id]}"])
+  conn.exec('DEALLOCATE patch')
+  patching
   redirect to("/memos/#{params[:id]}")
 end
 
 delete '/memos/:id' do
-  conn.exec ("DELETE FROM memo
-    WHERE id = #{params[:id]} ")
+  conn.prepare("delete", "DELETE FROM memo WHERE id = $1")
+  deleting = conn.exec_prepared("delete",["#{params[:id]}"])
+  conn.exec('DEALLOCATE delete')
+  deleting
   redirect to('/memos')
 end
