d4906e14e4e025d7b29ceb5b27ee9ff878b9359a
eatplaynap <eatplaynap329@gmail.com>
2021-06-13 21:00:32 +0900

    prepareの位置変更

--- memo_app.rb
+++ memo_app.rb
@@ -7,6 +7,12 @@ require 'pg'
 use Rack::MethodOverride
 
 conn = PG.connect(dbname: 'sinatra_memo_db', user: 'eatplaynap')
+conn.prepare('show', 'SELECT * FROM memo ORDER BY $1')
+conn.prepare("find", 'SELECT * FROM memo WHERE id = $1')
+conn.prepare('post', "INSERT INTO memo(memo_title, article) VALUES($1, $2)")
+conn.prepare('edit', 'SELECT * FROM memo WHERE id = $1')
+conn.prepare('patch', 'UPDATE memo SET memo_title = $1, article = $2 WHERE id = $3')
+conn.prepare('delete', 'DELETE FROM memo WHERE id = $1')
 
 helpers do
   def h(text)
@@ -16,9 +22,7 @@ end
 
 get '/memos' do
   @title = '一覧'
-  conn.prepare('show', 'SELECT * FROM memo ORDER BY $1')
   @memos = conn.exec_prepared('show', ['id'])
-  conn.exec("DEALLOCATE show")
   erb :index
 end
 
@@ -30,41 +34,30 @@ end
 post '/memos' do
   memo_title = params[:memo_title]
   article = params[:article]
-  conn.prepare('post', "INSERT INTO memo(memo_title, article)
-  VALUES($1, $2)")
   conn.exec_prepared('post', [memo_title.to_s, article.to_s])
-  conn.exec("DEALLOCATE post")
   redirect to('/memos')
 end
 
 get '/memos/:id' do
   @title = '詳細'
-  conn.prepare('find', 'SELECT * FROM memo WHERE id = $1')
-  @memo_info = conn.exec_prepared('find', [params[:id].to_s])
-  conn.exec("DEALLOCATE find")
+  @memo_info = conn.exec_prepared('find', ["#{params[:id]}"])
   erb :detail
 end
 
 get '/memos/:id/edit' do
   @title = '編集'
-  conn.prepare('edit', 'SELECT * FROM memo WHERE id = $1')
   @memo_info = conn.exec_prepared('edit', [params[:id].to_s])
-  conn.exec("DEALLOCATE edit")
   erb :edit
 end
 
 patch '/memos/:id' do
   memo_title = params[:memo_title]
   article = params[:article]
-  conn.prepare('patch', 'UPDATE memo SET memo_title = $1, article = $2 WHERE id = $3')
   conn.exec_prepared('patch', [memo_title.to_s, article.to_s, params[:id].to_s])
-  conn.exec("DEALLOCATE patch")
   redirect to("/memos/#{params[:id]}")
 end
 
 delete '/memos/:id' do
-  conn.prepare('delete', 'DELETE FROM memo WHERE id = $1')
   conn.exec_prepared('delete', [params[:id].to_s])
-  conn.exec("DEALLOCATE delete")
   redirect to('/memos')
 end
