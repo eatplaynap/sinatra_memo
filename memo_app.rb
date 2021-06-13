0f7d8b075d4b8330e0419a0ec244dc11b1ebc2d6
eatplaynap <eatplaynap329@gmail.com>
2021-06-13 16:29:25 +0900

    DEALLOCATEのシングルクオートをダブルクオートに修正

--- memo_app.rb
+++ memo_app.rb
@@ -18,7 +18,7 @@ get '/memos' do
   @title = '一覧'
   conn.prepare('show', 'SELECT * FROM memo ORDER BY $1')
   @memos = conn.exec_prepared('show', ['id'])
-  conn.exec('DEALLOCATE show')
+  conn.exec("DEALLOCATE show")
   erb :index
 end
 
@@ -33,7 +33,7 @@ post '/memos' do
   conn.prepare('post', "INSERT INTO memo(memo_title, article)
   VALUES($1, $2)")
   conn.exec_prepared('post', [memo_title.to_s, article.to_s])
-  conn.exec('DEALLOCATE post')
+  conn.exec("DEALLOCATE post")
   redirect to('/memos')
 end
 
@@ -41,7 +41,7 @@ get '/memos/:id' do
   @title = '詳細'
   conn.prepare('find', 'SELECT * FROM memo WHERE id = $1')
   @memo_info = conn.exec_prepared('find', [params[:id].to_s])
-  conn.exec('DEALLOCATE find')
+  conn.exec("DEALLOCATE find")
   erb :detail
 end
 
@@ -49,7 +49,7 @@ get '/memos/:id/edit' do
   @title = '編集'
   conn.prepare('edit', 'SELECT * FROM memo WHERE id = $1')
   @memo_info = conn.exec_prepared('edit', [params[:id].to_s])
-  conn.exec('DEALLOCATE edit')
+  conn.exec("DEALLOCATE edit")
   erb :edit
 end
 
@@ -58,13 +58,13 @@ patch '/memos/:id' do
   article = params[:article]
   conn.prepare('patch', 'UPDATE memo SET memo_title = $1, article = $2 WHERE id = $3')
   conn.exec_prepared('patch', [memo_title.to_s, article.to_s, params[:id].to_s])
-  conn.exec('DEALLOCATE patch')
+  conn.exec("DEALLOCATE patch")
   redirect to("/memos/#{params[:id]}")
 end
 
 delete '/memos/:id' do
   conn.prepare('delete', 'DELETE FROM memo WHERE id = $1')
   conn.exec_prepared('delete', [params[:id].to_s])
-  conn.exec('DEALLOCATE delete')
+  conn.exec("DEALLOCATE delete")
   redirect to('/memos')
 end
